0b2ea697cf4972d1e7d97492b37883a2c7c6cde9
mak <romanmak955@gmail.com>
2021-01-14 02:05:26 +0300

    add skipTest to maven-surefire

--- src/test/java/by/makarevich/Alla/s_wish_list/ApplicationTests.java
+++ src/test/java/by/makarevich/Alla/s_wish_list/ApplicationTests.java
@@ -2,10 +2,13 @@ package by.makarevich.Alla.s_wish_list;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootTest
 class ApplicationTests {
-
+	{
+		ApiContextInitializer.init();
+	}
 	@Test
 	void contextLoads() {
 	}
